2fc28c574bb82ed9f9f8d60385eac643bae9aa37
sibaprasad <sibap865@gmail.com>
2023-11-10 22:05:32 +0530

    requirements file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'HelmetDetection',
+    version= '0.0.0',
+    author= 'Sibaprasad Naik behera',
+    author_email= 'sibap865@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
